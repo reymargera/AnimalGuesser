f441d42e5dc76226f61a24d0f82be5f8a98602ff
Javier Vega <javierv2@uci.edu>
2015-06-03 15:45:41 -0700

    Adding the starting files

---
+++ FinalProject.c
@@ -0,0 +1,17 @@
+/*
+ * FinalProject.c
+ *
+ * Created: 6/3/2015 3:27:05 PM
+ *  Author: Javier Vega
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
