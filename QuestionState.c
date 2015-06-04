b4f123e9b11484c1bcdbb2a1806ae9c94c8c1196
chiahuang <chialh@uci.edu>
2015-06-03 20:51:15 -0700

    Implemented the questions into LCD
    Implemented the questions into LCD

---
+++ QuestionState.c
@@ -0,0 +1,45 @@
+//
+//  QuestionState.c
+//  
+//
+//  Created by Kay Lab on 6/3/15.
+//
+//
+
+#include "QuestionState.h"
+
+
+void askAQuestion() {
+    int numberOfQuestions = questionBank.sizeof();
+    bool animalTraits[][];
+    
+    for(int i=0; i < numberOfQuestions; i++)
+    {
+        pos_lcd(0,0);
+        sprintf(buffer, "Does your animal . . .");
+        puts_lcd2(buffer);
+        
+        pos_lcd(1,0);
+        sprintf(buffer, questionBank[i]);
+        puts_lcd2(buffer);
+        
+        // Question 5 so forth . . .
+        if(i>=5)
+        {
+            pos_lcd(0,0);
+            sprintf(buffer, "Is your animal . . .");
+            puts_lcd2(buffer);
+            
+            pos_lcd(1,0);
+            sprintf(buffer, questionBank[i]);
+            puts_lcd2(buffer);
+        }
+        
+        // If button(#1) pressed (Yes)
+        if(buttonPressed(0,4)) {
+            
+            
+        }
+    }
+
+}
--- QuestionState.h
+++ QuestionState.h
@@ -12,8 +12,9 @@
 #include "avr.h"
 #include "lcd.h"
 
-const char* questionBank[] = {"Have 4 legs?", "Walk on land?","Eat only meat", "Have claws?", "Move fast?",
+const char questionBank[] = {"Have 4 legs?", "Walk on land?","Eat only meat", "Have claws?", "Move fast?",
 							  "Domesticated?", "Found at a zoo?", "Eaten by people", "A mammal?", "Deadly?"};
+
 
 bool animalCharacteristics [7][10] = 
 {
